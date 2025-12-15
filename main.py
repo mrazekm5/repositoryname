ca028e60b706f7bc545edd033377adc253a23fb4
Pavel Kryl <pavel@kryl.eu>
2025-12-15 14:22:30 +0100

    uprava main a +data|config

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def pozdrav(jmeno):
-    return f"Ahoj, {jmeno}! Toto je verze 1.0."
+    return f"Ahoj, {jmeno}! Toto je verze 1.1. upraveno"
 
 if __name__ == "__main__":
     print(pozdrav("Svět Gitu"))
